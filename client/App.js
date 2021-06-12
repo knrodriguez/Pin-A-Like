7c8521894f1e6212c8ba704c4feca6096661491b
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-06-12 12:12:08 -0400

    feat: adds routes

--- client/App.js
+++ client/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Navbar } from './components'
+import { AllPinsView, PinView, NotFoundView } from './views'
 
 const App = () => {
     return (
-        <h1>Pin-A-Like</h1>
+        <Router>
+            <h1>Pin-A-Like</h1>
+            <Navbar />
+            {/* <Switch>
+                <Route exact path='/' render={() => <AllPinsView search='cats' />} />
+                <Route exact path='/cats' render={() => <AllPinsView search='cats' />}/>
+                <Route exact path='/pins/:pinId' component={PinView} />
+                <Route component={NotFoundView} />
+            </Switch> */}
+        </Router>
     )
 }
 
