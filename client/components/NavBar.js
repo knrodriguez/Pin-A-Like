4324f4b3f6fb3b72942e8eb1276742c7d9186a38
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-06-12 11:07:37 -0400

    chore: updates package.json metadata

---
+++ client/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+    return(
+        <div>
+            <a href='/home'
+            <img src='logo' onClick={}/>
+        </div>
+    )
+}
