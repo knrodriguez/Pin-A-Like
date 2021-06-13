aa8b46e0f12def317e34444340da4030bde29ad0
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-06-12 23:38:43 -0400

    refactor: reorganizes navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,17 +4,23 @@ import { Link } from 'react-router-dom'
 
 export default (props) => {
     return(
-        <div class='navbar' style={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
-            <Link to='/' style={{display:'flex', alignItems: 'center'}}>
-                <img src='/assets/images/pinterest_logo.png' alt='pinterest-logo' style={{textDecoration:'none', width:'30px', height:'auto'}}/>
-                Pin-A-Like
-            </Link>
-            <Link to='/'> Show me cats!</Link>
+        <div class='navbar'>
+            <div class='navbar'>
+                <div class='flex-row flex-align-center justify-start'>
+                    <Link to='/'>
+                        <img id='pinterest-logo' src='/assets/images/pinterest_logo.png' alt='pinterest-logo'/>
+                    </Link>
+                    <span class='padding-horizontal pinterest-text-decoration'>Pin-A-Like</span>
+                </div>
+            </div>
             <div>
                 {/* search bar */}
             </div>
-            <Link to='/404'>Login</Link>
-            <Link to='/404'>Sign Up</Link>
+            <div class='navbar navbar-group'>
+                <Link to='/'> Show me cats!</Link>
+                <Link to='/404'>Login</Link>
+                <Link to='/404'>Sign Up</Link>
+            </div>
         </div>
 
     )
